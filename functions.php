04dfc4235a4dad5f29ce65cf4e2921ccfaa38539
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-01-05 22:29:39 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,11 +2,12 @@
 	include 'config.php';
 	function getPlayerStats( $platform, $epicNickname ){
 		$apiUrlPlayerStatsEndpoint = 'https://api.fortnitetracker.com/v1/profile/' . $platform . '/' . $epicNickname;
+		//$apiUrlPlayerStatsEndpoint = 'https://api.fortnitetracker.com/v1/powerrankings/' . $platform . '/' . $region. '/' . $epic;
 		$ch = curl_init();// Inicializar cURL
 		curl_setopt( $ch, CURLOPT_URL, $apiUrlPlayerStatsEndpoint );// Establecer URL
 		curl_setopt( $ch, CURLOPT_HTTPHEADER, array(// Establecer cabeçalhos
 			'TRN-Api-Key:' . FN_API_KEY// Adicionar a chave da API
-		) );
+		));
 		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, TRUE );// Retornar o resultado
 		curl_setopt( $ch, CURLOPT_HEADER, FALSE );// Não exibir o cabeçalho
 		curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, FALSE );// Não verificar o certificado SSL
