15329eb6e8331075475e3254595393184d690203
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-03-17 19:40:04 -0300

    Update detalhes_cosmetico.php

--- detalhes_cosmetico.php
+++ detalhes_cosmetico.php
@@ -14,7 +14,9 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Detalhes do Cosmético</title>
+        <link rel="icon" href="./IMG/favicon.png" type="image/x-icon" />
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
         <link href="./CSS/style.css" rel="stylesheet">
     </head>
     <body>
@@ -25,9 +27,32 @@
                     <img src="<?php echo htmlspecialchars($cosmeticData['data']['images']['icon'], ENT_QUOTES, 'UTF-8'); ?>" alt="Imagem do Cosmético" class="img-fluid">
                 </div>
                 <div class="col-md-6">
-                    <h3>Descrição</h3>
+                    <h3 class="h3-cor">Descrição</h3>
                     <p><?php echo htmlspecialchars($cosmeticData['data']['description'], ENT_QUOTES, 'UTF-8'); ?></p>
-                    <!-- Adicione mais detalhes conforme necessário -->
+                    <h3 class="h3-cor">Tipo</h3>
+                    <p><?php echo htmlspecialchars($cosmeticData['data']['type']['displayValue'], ENT_QUOTES, 'UTF-8'); ?></p>
+                    <h3 class="h3-cor">Raridade</h3>
+                    <p><?php echo htmlspecialchars($cosmeticData['data']['rarity']['displayValue'], ENT_QUOTES, 'UTF-8'); ?></p>
+                    <h3 class="h3-cor">Set</h3>
+                    <p><?php echo htmlspecialchars($cosmeticData['data']['set']['text'], ENT_QUOTES, 'UTF-8'); ?></p>
+                    <h3 class="h3-cor">Introdução</h3>
+                    <p><?php echo "Capítulo" . " " . htmlspecialchars($cosmeticData['data']['introduction']['chapter'], ENT_QUOTES, 'UTF-8') . ", " . "Temporada" . " " . htmlspecialchars($cosmeticData['data']['introduction']['season'], ENT_QUOTES, 'UTF-8'); ?></p>
+                    <h3 class="h3-cor">Variantes</h3>
+                    <?php if (!empty($cosmeticData['variants'])): ?>
+                    <?php foreach ($cosmeticData['variants'] as $variant): ?>
+                    <h4><?php echo htmlspecialchars($variant['channel'] . " - " . $variant['type'], ENT_QUOTES, 'UTF-8'); ?></h4>
+                    <div class="options-container">
+                        <?php foreach ($variant['options'] as $option): ?>
+                        <div class="option">
+                            <h5><?php echo htmlspecialchars($option['name'], ENT_QUOTES, 'UTF-8'); ?></h5>
+                            <img src="<?php echo htmlspecialchars($option['image'], ENT_QUOTES, 'UTF-8'); ?>" alt="Imagem da variante">
+                        </div>
+                        <?php endforeach; ?>
+                    </div>
+                    <?php endforeach; ?>
+                    <?php else: ?>
+                    <p>Não há variantes disponíveis para este cosmético.</p>
+                    <?php endif; ?>
                 </div>
             </div>
             <a href="cosmeticos.php" class="btn btn-primary">Voltar aos Cosméticos</a>
