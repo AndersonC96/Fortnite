005df13603ad51e10a1bb1d519bb1176b82269d5
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-03-17 18:43:23 -0300

    Update noticias_br.php

--- noticias_br.php
+++ noticias_br.php
@@ -56,29 +56,30 @@
             </div>
         </nav>
         <div class="container mt-4">
-        <div class="container mt-4">
-    <h1>Notícias</h1>
-    <div class="row">
-        <?php
-            $newsData = callFortniteAPI('news/br');
-            if($newsData && $newsData['status'] == 200 && !empty($newsData['data']['br']['motds'])){
-                foreach($newsData['data']['br']['motds'] as $newsItem){
-                    echo "<div class='col-md-6'>";
-                    echo "<div class='card'>";
-                    echo "<img class='card-img-top' src='" . htmlspecialchars($newsItem['image'], ENT_QUOTES, 'UTF-8') . "' alt='Imagem da notícia'>";
-                    echo "<div class='card-body'>";
-                    echo "<h5 class='card-title'>" . htmlspecialchars($newsItem['title'], ENT_QUOTES, 'UTF-8') . "</h5>";
-                    echo "<p class='card-text'>" . htmlspecialchars($newsItem['body'], ENT_QUOTES, 'UTF-8') . "</p>";
-                    echo "</div>";
-                    echo "</div>";
-                    echo "</div>";
-                }
-            }else{
-                echo "<p>Não foi possível carregar as notícias.</p>";
-            }
-        ?>
-    </div>
-</div>
+            <div class="container mt-4">
+                <h1>Notícias</h1>
+                <div class="row">
+                    <?php
+                        $newsData = callFortniteAPI('news/br');
+                        if($newsData && $newsData['status'] == 200 && !empty($newsData['data']['br']['motds'])){
+                            foreach($newsData['data']['br']['motds'] as $newsItem){
+                                echo "<div class='col-md-6'>";
+                                echo "<div class='card'>";
+                                echo "<img class='card-img-top' src='" . htmlspecialchars($newsItem['image'], ENT_QUOTES, 'UTF-8') . "' alt='Imagem da notícia'>";
+                                echo "<div class='card-body'>";
+                                echo "<h5 class='card-title'>" . htmlspecialchars($newsItem['title'], ENT_QUOTES, 'UTF-8') . "</h5>";
+                                echo "<p class='card-text'>" . htmlspecialchars($newsItem['body'], ENT_QUOTES, 'UTF-8') . "</p>";
+                                echo "</div>";
+                                echo "</div>";
+                                echo "</div>";
+                            }
+                        }else{
+                            echo "<p>Não foi possível carregar as notícias.</p>";
+                        }
+                    ?>
+                </div>
+            </div>
+        </div>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
