902567a93b9dc588cbe57690b92a5d53f8ad2526
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-04-01 15:50:08 -0300

    Update plataformas.php

--- plataformas.php
+++ plataformas.php
@@ -47,62 +47,36 @@
         </nav>
         <div class="container mt-4">
             <h1>Plataformas Suportadas</h1>
-            <div class="row">
-                <div class="col">
-                    <a href="https://www.fortnite.com/mobile/android/new-device" target="_blank">
-                        <img src="./IMG/android.png" alt="Android" class="img-fluid">
-                    </a>
-                </div>
-                <div class="col">
-                    <a href="https://luna.amazon.com/detail/B0BTRVRN6M?r=web&ref=tmp_ext_epic&autoplay=1" target="_blank">
-                        <img src="./IMG/amazon-luna.png" alt="Amazon Luna" class="img-fluid">
-                    </a>
-                </div>
-                <div class="col">
-                    <a href="https://store.epicgames.com/pt-BR/discover/fortnite" target="_blank">
-                        <img src="./IMG/epic-games.png" alt="Epic Games" class="img-fluid">
-                    </a>
-                </div>
-                <div class="col">
-                    <a href="https://www.nintendo.com/us/store/products/fortnite-switch/" target="_blank">
-                        <img src="./IMG/switch.png" alt="Nintendo Switch" class="img-fluid">
-                    </a>
-                </div>
-                <div class="col">
-                    <a href="https://www.nvidia.com/en-us/geforce-now/fortnite-mobile/?ncid=partn-629709#cid=ct0021_partn_en-us" target="_blank">
-                        <img src="./IMG/geforce-now.png" alt="Nvidia Geforce Now" class="img-fluid">
-                    </a>
-                </div>
-                <div class="col">
-                    <a href="https://store.playstation.com/pt-br/product/UP1477-PPSA01922_00-FNBNDL0000000000" target="_blank">
-                        <img src="./IMG/ps4.png" alt="Playstation 4" class="img-fluid">
-                    </a>
-                </div>
-                <div class="col">
-                    <a href="https://store.playstation.com/pt-br/product/UP1477-PPSA01922_00-FNBNDL0000000000" target="_blank">
-                        <img src="./IMG/ps5.png" alt="Playstation 5" class="img-fluid">
-                    </a>
-                </div>
-                <div class="col">
-                    <a href="https://galaxystore.samsung.com/detail/com.epicgames.portal?session_id=W_96efd9e9bb72fdd7a126fdbfe6de7871" target="_blank">
-                        <img src="./IMG/galaxy-store.png" alt="Samsung Galaxy Store" class="img-fluid">
-                    </a>
-                </div>
-                <div class="col">
-                    <a href="https://www.xbox.com/pt-BR/play/launch/fortnite/BT5P2X999VH2" target="_blank">
-                        <img src="./IMG/xcloud.png" alt="Xbox Cloud Gaming" class="img-fluid">
-                    </a>
-                </div>
-                <div class="col">
-                    <a href="https://www.xbox.com/pt-BR/play/launch/fortnite/BT5P2X999VH2" target="_blank">
-                        <img src="./IMG/one.png" alt="Xbox One" class="img-fluid">
-                    </a>
-                </div>
-                <div class="col">
-                    <a href="https://www.xbox.com/pt-BR/play/launch/fortnite/BT5P2X999VH2" target="_blank">
-                        <img src="./IMG/series.png" alt="Xbox Series" class="img-fluid">
-                    </a>
-                </div>
+            <br>
+            <h2>Dispositivos Móveis</h2>
+            <div class="row mb-4">
+                <div class="col"><a href="https://www.fortnite.com/mobile/android/new-device"><img src="./IMG/android.png" alt="Android" class="img-platform"></a></div>
+                <div class="col"><a href="https://luna.amazon.com/detail/B0BTRVRN6M?r=web&ref=tmp_ext_epic&autoplay=1"><img src="./IMG/amazon-luna.png" alt="Amazon Luna" class="img-platform"></a></div>
+                <div class="col"><a href="https://www.nvidia.com/en-us/geforce-now/fortnite-mobile/?ncid=partn-629709#cid=ct0021_partn_en-us" target="_blank"><img src="./IMG/geforce-now.png" alt="Nvidia Geforce Now" class="img-platform"></a></div>
+                <div class="col"><a href="https://galaxystore.samsung.com/detail/com.epicgames.portal?session_id=W_96efd9e9bb72fdd7a126fdbfe6de7871" target="_blank"><img src="./IMG/galaxy-store.png" alt="Samsung Galaxy Store" class="img-platform"></a></div>
+                <div class="col"><a href="https://www.xbox.com/pt-BR/play/launch/fortnite/BT5P2X999VH2" target="_blank"><img src="./IMG/xcloud.png" alt="Xbox Cloud Gaming" class="img-platform"></a></div>
+            </div>
+            <br>
+            <h2>Nintendo</h2>
+            <div class="row mb-4">
+                <div class="col"><a href="https://www.nintendo.com/us/store/products/fortnite-switch/"><img src="./IMG/switch.png" alt="Nintendo Switch" class="img-platform"></a></div>
+            </div>
+            <br>
+            <h2>PC</h2>
+            <div class="row mb-4">
+                <div class="col"><a href="https://store.epicgames.com/pt-BR/discover/fortnite"><img src="./IMG/epic-games.png" alt="PC" class="img-platform"></a></div>
+            </div>
+            <br>
+            <h2>PlayStation</h2>
+            <div class="row mb-4">
+                <div class="col"><a href="https://store.playstation.com/pt-br/product/UP1477-PPSA01922_00-FNBNDL0000000000"><img src="./IMG/ps4.png" alt="Playstation 4" class="img-platform"></a></div>
+                <div class="col"><a href="https://store.playstation.com/pt-br/product/UP1477-PPSA01922_00-FNBNDL0000000000"><img src="./IMG/ps5.png" alt="Playstation 5" class="img-platform"></a></div>
+            </div>
+            <br>
+            <h2>Xbox</h2>
+            <div class="row mb-4">
+                <div class="col"><a href="https://www.xbox.com/pt-BR/play/launch/fortnite/BT5P2X999VH2"><img src="./IMG/one.png" alt="Xbox One" class="img-platform"></a></div>
+                <div class="col"><a href="https://www.xbox.com/pt-BR/play/launch/fortnite/BT5P2X999VH2"><img src="./IMG/series.png" alt="Xbox Series" class="img-platform"></a></div>
             </div>
         </div>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
