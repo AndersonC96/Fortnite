c7ec20b45be716747bebc00a2ccc2649c98542ab
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-04-01 14:47:51 -0300

    Update status.php

--- status.php
+++ status.php
@@ -5,6 +5,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Status do Fortnite</title>
+        <link rel="icon" href="./IMG/favicon.png" type="image/x-icon" />
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
         <link href="./CSS/style.css" rel="stylesheet">
     </head>
