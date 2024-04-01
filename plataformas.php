ac048f095ca503fe6f56f288e200abf9d49bb4d9
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-04-01 15:34:19 -0300

    Create plataformas.php

---
+++ plataformas.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Plataformas Fortnite</title>
+        <link rel="icon" href="./IMG/favicon.png" type="image/x-icon" />
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+        <link href="./CSS/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="index.php">
+                <img src="./IMG/logo.png" alt="Logo do Fortnite">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Notícias</a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="noticias.php">Notícias Gerais</a>
+                            <a class="dropdown-item" href="noticias_br.php">Notícias BR</a>
+                            <a class="dropdown-item" href="noticias_stw.php">Notícias STW</a>
+                            <a class="dropdown-item" href="noticias_creative.php">Notícias Creative</a>
+                        </div>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="cosmeticos.php">Cosméticos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="loja.php">Loja</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="status.php">Status</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="plataformas.php">Plataformas</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="container mt-4">
+            <h1>Plataformas Suportadas</h1>
+            <div class="row">
+                <div class="col">
+                    <a href="https://www.fortnite.com/mobile/android/new-device" target="_blank">
+                        <img src="./IMG/android.png" alt="Android" class="img-fluid">
+                    </a>
+                </div>
+                <div class="col">
+                    <a href="https://luna.amazon.com/detail/B0BTRVRN6M?r=web&ref=tmp_ext_epic&autoplay=1" target="_blank">
+                        <img src="./IMG/amazon-luna.png" alt="Amazon Luna" class="img-fluid">
+                    </a>
+                </div>
+                <div class="col">
+                    <a href="https://store.epicgames.com/pt-BR/discover/fortnite" target="_blank">
+                        <img src="./IMG/epic-games.png" alt="Epic Games" class="img-fluid">
+                    </a>
+                </div>
+                <div class="col">
+                    <a href="https://www.nintendo.com/us/store/products/fortnite-switch/" target="_blank">
+                        <img src="./IMG/switch.png" alt="Nintendo Switch" class="img-fluid">
+                    </a>
+                </div>
+                <div class="col">
+                    <a href="https://www.nvidia.com/en-us/geforce-now/fortnite-mobile/?ncid=partn-629709#cid=ct0021_partn_en-us" target="_blank">
+                        <img src="./IMG/geforce-now.png" alt="Nvidia Geforce Now" class="img-fluid">
+                    </a>
+                </div>
+                <div class="col">
+                    <a href="https://store.playstation.com/pt-br/product/UP1477-PPSA01922_00-FNBNDL0000000000" target="_blank">
+                        <img src="./IMG/ps4.png" alt="Playstation 4" class="img-fluid">
+                    </a>
+                </div>
+                <div class="col">
+                    <a href="https://store.playstation.com/pt-br/product/UP1477-PPSA01922_00-FNBNDL0000000000" target="_blank">
+                        <img src="./IMG/ps5.png" alt="Playstation 5" class="img-fluid">
+                    </a>
+                </div>
+                <div class="col">
+                    <a href="https://galaxystore.samsung.com/detail/com.epicgames.portal?session_id=W_96efd9e9bb72fdd7a126fdbfe6de7871" target="_blank">
+                        <img src="./IMG/galaxy-store.png" alt="Samsung Galaxy Store" class="img-fluid">
+                    </a>
+                </div>
+                <div class="col">
+                    <a href="https://www.xbox.com/pt-BR/play/launch/fortnite/BT5P2X999VH2" target="_blank">
+                        <img src="./IMG/xcloud.png" alt="Xbox Cloud Gaming" class="img-fluid">
+                    </a>
+                </div>
+                <div class="col">
+                    <a href="https://www.xbox.com/pt-BR/play/launch/fortnite/BT5P2X999VH2" target="_blank">
+                        <img src="./IMG/one.png" alt="Xbox One" class="img-fluid">
+                    </a>
+                </div>
+                <div class="col">
+                    <a href="https://www.xbox.com/pt-BR/play/launch/fortnite/BT5P2X999VH2" target="_blank">
+                        <img src="./IMG/series.png" alt="Xbox Series" class="img-fluid">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    </body>
+</html>
